f622630cd71d4d86ef47a34a8cebc531b805159e
Vinicius Perillo <vinicius.perillo25@gmail.com>
2024-07-28 01:38:41 -0300

    feat: Criação do backbone_extraction

--- network.py
+++ network.py
@@ -3,6 +3,8 @@ import json
 import itertools
 import networkx as nx
 import pickle
+import scipy.integrate as integrate
+import numpy as np
 
 MAP = {
     'Não': -1,
@@ -22,13 +24,13 @@ def estruturar_rede():
         votos_dep2 = pd.Series(deputados[id_dep2]['votos'])
 
         intersec = votos_dep1.index.intersection(votos_dep2.index)
-        weight = (votos_dep1[intersec] == votos_dep2[intersec]).sum()
+        concordancia = (votos_dep1[intersec] == votos_dep2[intersec]).sum()/len(intersec)
         
-        if weight > 100:
-            print(f'({id_dep1})<={weight}=>({id_dep2})', flush= True)
+        if concordancia > 100:
+            print(f'({id_dep1})<={concordancia}=>({id_dep2})', flush= True)
 
             try:
-                graph.add_edge(id_dep1, id_dep2, weight= weight)
+                graph.add_edge(id_dep1, id_dep2, concordancia= concordancia)
             except KeyError:
                 graph.add_nodes_from(
                     id_dep1, 
@@ -48,7 +50,59 @@ def estruturar_rede():
             #
         #
 
-    nx.write_graphml(graph, 'teste.graphml')
+    return graph
+
+def calculate_strength(graph: nx.Graph):
+    
+    nodes = graph.nodes(data=True)
+    
+    for node, edges in graph.adjacency():
+        strength = 0
+
+        for edge in edges.values:
+            strength += edge['concordancia']
+        
+        node_info = nodes[node]
+        node_info |= {'strength': strength}
+        graph.add_node(node_info)
 
 
-estruturar_rede()
+
+def backbone_extraction(graph: nx.Graph, threshold):
+    
+    calculate_strength(graph)
+
+    nodes = graph.nodes(data=True)
+
+    for edge in graph.edges():
+        edge_data = graph.get_edge_data(*edge)
+        
+        weight = edge_data['concordancia'] / nodes[edge[1]]['strength']
+        degree = graph.degree[edge[1]]
+
+        probability = (degree - 1) * (1 - np.power(1 - weight), degree + 1) / (degree + 1)
+
+        edge_data |= {'probability': probability}
+        graph.add_edge(*edge, edge_data)
+
+
+    n_size = len(graph)
+    less_probably_edges = iter(sorted(graph.edges(data=True), key= lambda x: x[2]['probability']))
+
+    continue_backbone = True
+    while continue_backbone:
+        edge = next(less_probably_edges)
+        graph.remove_edge(*edge)
+
+        components = nx.connected_components(graph)
+        largest_component = max(components, key=len)
+        
+        if len(largest_component) / n_size <= 0.8:
+            continue_backbone = False
+
+graph = estruturar_rede()
+backbone_extraction(graph)
+
+nx.write_graphml(graph, 'teste.gml')
+
+
