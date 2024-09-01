4a7c6eaa4dad08a85ca6f8e61bfd3de9403e4a74
jimiHendrix <vinicius.perillo25@gmail.com>
2024-09-01 00:06:20 -0300

    feat: arquivo de plot

---
+++ plot.py
@@ -0,0 +1,19 @@
+from forceatlas2 import forceatlas2
+import networkx as nx
+import ipysigma as sigma
+import pickle
+
+
+def _apply_force_atlas(graph: nx.Graph):
+    I = forceatlas2(G=nx.to_numpy_array(graph,  weight= 'concordancia'))
+
+    positions_dict = {node:{'x':I[i][0], 'y':I[i][1]} for i, node in enumerate(graph.nodes)}
+
+    nx.set_node_attributes(graph, positions_dict)
+
+def plot_nework(network_name: str):
+    with open(f'data/{network_name}.pkl', 'rb') as file:
+        graph = pickle.load(file)
+        
+    _apply_force_atlas(graph)
+    sigma.Sigma(graph, node_color= "partido", node_label= 'nome').to_html(f'./data/{network_name}.html')
