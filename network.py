7cf84653c7b3a5ed453b7b201581484fd77ebf8f
jimiHendrix <vinicius.perillo25@gmail.com>
2024-09-16 22:19:42 -0300

    fix: corrigido problema do valor das arestas

--- network.py
+++ network.py
@@ -11,7 +11,7 @@ MAP_VOTOS = {
     "Sim": 1,
     "Não": -1,
     "Abstenção": 0,
-    "Obstrução": 0
+    "Obstrução": -1
 }
 
 def estruturar_rede(deputados: dict) -> nx.Graph:
@@ -19,35 +19,35 @@ def estruturar_rede(deputados: dict) -> nx.Graph:
     graph = nx.Graph()
 
     for id_dep1, id_dep2 in itertools.combinations(deputados.keys(), 2):
-        votos_dep1 = pd.Series(deputados[id_dep1]['votos']).map(MAP_VOTOS)
-        votos_dep2 = pd.Series(deputados[id_dep2]['votos']).map(MAP_VOTOS)
+        votos_dep1 = pd.Series(deputados[id_dep1]['votos']).dropna().map(MAP_VOTOS)
+        votos_dep2 = pd.Series(deputados[id_dep2]['votos']).dropna().map(MAP_VOTOS)
 
         intersec = votos_dep1.index.intersection(votos_dep2.index)
-        print
 
         if len(intersec) > 0:
-            concordancia = np.float64((votos_dep1[intersec] == votos_dep2[intersec]).sum()/len(intersec))
+            concordancia = np.float64((votos_dep1[intersec] * votos_dep2[intersec]).sum()) / len(intersec)
 
-            if not id_dep1 in graph:
-                graph.add_node(
-                    id_dep1, 
-                    nome= deputados[id_dep1]['ultimoStatus']['nomeEleitoral'],
-                    partido=  deputados[id_dep1]['ultimoStatus']['siglaPartido'],
-                    uf=  deputados[id_dep1]['ultimoStatus']['siglaUf'],
-                    condicao=  deputados[id_dep1]['ultimoStatus']['condicaoEleitoral']
-                    
-                )
+            if concordancia > 0:
+                if not id_dep1 in graph:
+                    graph.add_node(
+                        id_dep1, 
+                        nome= deputados[id_dep1]['ultimoStatus']['nomeEleitoral'],
+                        partido=  deputados[id_dep1]['ultimoStatus']['siglaPartido'],
+                        uf=  deputados[id_dep1]['ultimoStatus']['siglaUf'],
+                        condicao=  deputados[id_dep1]['ultimoStatus']['condicaoEleitoral']
+                        
+                    )
 
-            if not id_dep2 in graph:
-                graph.add_node(
-                    id_dep2, 
-                    nome= deputados[id_dep2]['ultimoStatus']['nomeEleitoral'],
-                    partido=  deputados[id_dep2]['ultimoStatus']['siglaPartido'],
-                    uf=  deputados[id_dep2]['ultimoStatus']['siglaUf'],
-                    condicao=  deputados[id_dep2]['ultimoStatus']['condicaoEleitoral']
-                )
-            
-            graph.add_edge(id_dep1, id_dep2, concordancia= concordancia)
+                if not id_dep2 in graph:
+                    graph.add_node(
+                        id_dep2, 
+                        nome= deputados[id_dep2]['ultimoStatus']['nomeEleitoral'],
+                        partido=  deputados[id_dep2]['ultimoStatus']['siglaPartido'],
+                        uf=  deputados[id_dep2]['ultimoStatus']['siglaUf'],
+                        condicao=  deputados[id_dep2]['ultimoStatus']['condicaoEleitoral']
+                    )
+                
+                graph.add_edge(id_dep1, id_dep2, concordancia= concordancia)
             #
         #
 
@@ -55,7 +55,7 @@ def estruturar_rede(deputados: dict) -> nx.Graph:
 
 
 def network(file_name: str):
-    print_log("ESTRUTURAÇÃO DA REDE-------------------")
+    print_log(f"{'NETWORK':<10}:ESTRUTURAÇÃO DA REDE-------------------")
     
     deputados = load_json(file_name + "_deputados")
     graph = estruturar_rede(deputados)
